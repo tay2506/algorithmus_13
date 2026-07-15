0079d0193af9d1afb0dba1cb1a8d51d208065524
𝓣𝓪𝔂 <tauanyrodrigues422@gmail.com>
2026-07-15 09:21:24 -0300

    Update algorithmus.js

--- algorithmus.js
+++ algorithmus.js
@@ -1,23 +1,21 @@
-alert("vamos ver a média da nota?");
-let nota1 = parseFloat(prompt("Digite a nota 1:"));
-let nota2 = parseFloat(prompt("Digite a nota 2:"));
-let media = (nota1 + nota2) / 2;
-let mensagem;
-if (media>=0 && media<= 4.5) {
-    mensagem = "Reprovado:" + media;
-}
-else if (media>=4.6 && media<= 5.9) {
+alert("Calcule sua média."); 
+let num1 = parseFloat(prompt("Digite a nota 1:"));
+let num2 = parseFloat(prompt("Digite a nota 2:")); 
+let media = (num1 + num2) / 2; 
+if (media >= 0 && media <= 4.5){
+    mensagem = "Reprovado: " + media; 
+} 
+else if (media >= 4.6 && media <= 5.9){
     mensagem = "Aprovado por média: " + media;
-}
-else if (media>= 6 && media<= 9.9) {
+} 
+else if (media >= 6 && media <= 9.9){
     mensagem = "Aprovado, acima da média: " + media;
-}
-else if (media==10) {
-    mensagem = "Aprovado com média máxima, Parabéns!: " + media;
-}
+} 
+else if (media == 10){
+    mensagem = "Aprovado com média máxima. Parabéns! " + media;
+} 
 else {
-    mensagem = "Nota inválida";
-}
+    mensagem = "Nota inválida"; 
+} 
 alert(mensagem);
-
 document.write( "<p style='font-size: 2rem;'>" + mensagem + "</p>" );
